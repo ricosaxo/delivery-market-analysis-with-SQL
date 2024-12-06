263f09b00b50d3425d3ae7262e8605b2cdb6ee7b
Your Name <your.email@example.com>
2024-12-06 11:26:36 +0100

    update-2

--- Urson/uber_eats_urson.sql
+++ Urson/uber_eats_urson.sql
@@ -4,3 +4,8 @@ SELECT title, sanitized_title slug,
 FROM restaurants
 WHERE CAST(rating__review_count AS INTEGER) > 100
 ORDER BY rating__rating_value DESC;
+
+SELECT location_city, COUNT(*) AS restaurant_count
+FROM restaurants
+GROUP BY location__city 
+ORDER BY restaurant_count DESC;
